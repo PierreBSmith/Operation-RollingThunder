d518aa91b2edc68fa86dcbb75d6673c19fa8c497
jeongmin-ahn122 <jeongmin.ahn.122@gmail.com>
2021-01-29 23:03:58 -0800

    Copying things. Movement for head and torso

---
+++ Assets/Scripts/Movement/TorsoMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorsoMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpMultiplier;
+
+    private Rigidbody2D _r2D;
+    private bool grounded;
+
+    private Vector2 force;
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.tag == "Floor")
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        grounded = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _r2D = GetComponent<Rigidbody2D>();
+        grounded = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float input = Input.GetAxis("Horizontal");
+        if (input != 0 && grounded)
+        {
+            _r2D.velocity = new Vector2(speed * input, jumpMultiplier);
+            grounded = false;
+        }
+    }
+}
